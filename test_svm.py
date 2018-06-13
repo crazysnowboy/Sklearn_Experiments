68600b2bf010a800762f0610c74f8ab7136c9e8b
kailin <kailin@ulsee.com>
2018-06-13 16:30:16 +0800

    init

---
+++ test_svm.py
@@ -0,0 +1,33 @@
+from sklearn import datasets
+import cv2
+
+from sklearn import svm
+
+def Test():
+    iris = datasets.load_iris()
+    digits = datasets.load_digits()
+
+    clf = svm.SVC(gamma=0.001, C=100.)
+
+    train_images = digits.data[:-1]
+    train_lables = digits.target[:-1]
+
+    clf.fit(train_images,train_lables)  
+
+    test_image  = digits.data[-1:]
+    res = clf.predict(test_image)
+
+    print "shape of image = ",train_images.shape
+    print "shape of lables = ",train_lables.shape
+    print "shape of test_image = ",test_image.shape
+
+    print "res = ",res
+
+    cv2.namedWindow('test_image',0)
+    test_image = test_image.reshape(8,8)/test_image.max()
+    cv2.imshow('test_image',test_image)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    
+    Test()
